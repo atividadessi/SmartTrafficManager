d4e701069a05b7f609551315376c5feaf05e9389
vinicciusnev <xxnashflows@hotmail.com>
2024-10-10 19:32:08 -0300

    feat: Atualizando main

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import logging
 from src.controller.TrafficController import TrafficController
+from src.controller.strategy.TrafficControlStrategy import BasicTrafficControlStrategy
+from src.view.observer.TrafficLightViewObserver import TrafficLightView
 
 if __name__ == "__main__":
+    # Configuração do logger
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
     logging.info('Iniciando o sistema de controle de tráfego.')
 
-    # Tempo de simulação em segundos
-    controller = TrafficController()
+    controller = TrafficController(BasicTrafficControlStrategy(), TrafficLightView())
+
     controller.run()
-
-
 
     logging.info('Sistema finalizado.')
